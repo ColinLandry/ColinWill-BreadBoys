b05561d5df33167bd3ed0f595dd21c9051f42850
Colin Landry <colin.landry01@stclairconnect.ca>
2017-12-07 16:26:53 -0500

    Created layout for contact page: #4 #20 * Created buttons for every intent option * Created dimens.xml values for general purpose use * Started creating phone intent

--- app/src/main/java/com/breadboys/willcolin/MainActivity.java
+++ app/src/main/java/com/breadboys/willcolin/MainActivity.java
@@ -18,7 +18,7 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener,
-                    OrderFragment.OnFragmentInteractionListener,
+                    CheckoutFragment.OnFragmentInteractionListener,
                     LoavesFragment.OnFragmentInteractionListener,
                     RecipeFragment.OnFragmentInteractionListener,
                     SettingsFragment.OnFragmentInteractionListener{
@@ -107,8 +107,8 @@ public class MainActivity extends AppCompatActivity
             transaction.replace(R.id.content, new RecipeFragment());
             transaction.addToBackStack(null);
             transaction.commit();
-        } else if (id == R.id.nav_order) {
-            transaction.replace(R.id.content, new OrderFragment());
+        } else if (id == R.id.nav_checkout) {
+            transaction.replace(R.id.content, new CheckoutFragment());
             transaction.addToBackStack(null);
             transaction.commit();
         } else if (id == R.id.nav_contact) {
