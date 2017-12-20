f71231d4a3e5ffe620715c038f2dcd60cf712496
Will Coulter <will.coulter01@stclairconnect.ca>
2017-12-20 09:13:42 -0500

    Created an empty intent

--- app/src/main/java/com/breadboys/willcolin/CheckoutFragment.java
+++ app/src/main/java/com/breadboys/willcolin/CheckoutFragment.java
@@ -1,12 +1,17 @@
 package com.breadboys.willcolin;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.breadboys.willcolin.JavaBeans.Loaf;
 
 
 /**
@@ -26,6 +31,10 @@ public class CheckoutFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    Button orderBtn;
+    Button calendarBtn;
+    Button locationBtn;
 
     private OnFragmentInteractionListener mListener;
 
@@ -63,11 +72,31 @@ public class CheckoutFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_checkout, container, false);
+            // Inflate the layout for this fragment
+            View view = inflater.inflate(R.layout.fragment_recipe_page, container, false);
+            TextView totalCost = (TextView) view.findViewById(R.id.totalCostText);
+            totalCost.setText("rr");
+
+            calendarBtn = (Button) view.findViewById(R.id.calendarBtn);
+            locationBtn = (Button) view.findViewById(R.id.locationBtn);
+
+        locationBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                
+
+
+                if(i.resolveActivity(getActivity().getPackageManager()) != null){
+                    startActivity(i);
+                }
+            }
+        });
+
+            return view;
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
+    // TODO: Rename method, update  argument and hook method into UI event
     public void onButtonPressed(Uri uri) {
         if (mListener != null) {
             mListener.onFragmentInteraction(uri);
